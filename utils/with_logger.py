8762e667261f6fffac78cf2550df95c31a59d510
TheConnMan <bcconn2112@gmail.com>
2017-12-16 13:15:54 -0500

    Add stdout logging

--- utils/with_logger.py
+++ utils/with_logger.py
@@ -1,7 +1,14 @@
 import logging
+import sys
 
 
 class WithLogger(object):
     @property
     def logger(self):
-        return logging.getLogger(type(self).__name__)
+        logger = logging.getLogger(type(self).__name__)
+        formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+        ch = logging.StreamHandler(sys.stdout)
+        ch.setLevel(logging.DEBUG)
+        ch.setFormatter(formatter)
+        logger.addHandler(ch)
+        return logger
